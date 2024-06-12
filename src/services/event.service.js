ba638fa1b5881e24e25a94f1e6dcb512c0ba6c2b
Andres Suarique <andresuarique@gmail.com>
2024-06-11 20:36:38 -0500

    Events service implementation

--- src/services/event.service.js
+++ src/services/event.service.js
@@ -1,5 +1,39 @@
+import { models } from '../libs/sequelize.js';
+import  boom  from "@hapi/boom";
+import bcrypt from 'bcrypt';
 
 class EventsService{
+    constructor(){
+    }
+    async create(body){
+        const event = await models.Event.create(body);
+        return event;
+    }
 
+    async find(){
+        const rta = await models.Event.findAll();
+        return rta;
+    }
+
+    async findOne(id){
+        const rta = await models.Event.findByPk(id);
+        if (!rta) {
+            throw boom.notFound('event not found');
+          }
+        return rta;
+    }
+
+    async update(id, body){
+        const event = await this.findOne(id);
+        const rta = await event.update(body);
+        return rta;
+
+    }
+
+    async delete(id){
+        const event = await this.findOne(id);
+        const rta = await event.destroy();
+        return {id};
+    }
 }
 export {EventsService};
